321e9606507999c5346685ad46739cf1ece61c66
Danilo Araujo de Souza <danilo.asouza@hotmail.com>
2023-02-20 01:07:09 -0300

    COrreção

--- lib/src/core/config/env/env.dart
+++ lib/src/core/config/env/env.dart
@@ -1,5 +1,4 @@
 class Env {
   static const backendBaseUrl = 'https://api.openai.com/v1';
-  static const backendToken =
-      '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';
+  static const backendToken = 'SUA KEY';
 }
